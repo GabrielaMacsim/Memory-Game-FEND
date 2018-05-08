db9e13f1b892b4e9a66e983832729253c982cdd6
Gabriela Macsim <anagabrielamacsim@gmail.com>
2018-05-08 16:33:07 +0300

    Made a list of all card images

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*
- * Create a list that holds all of your cards
+ * A list that holds all cards
  */
 
+var arrayOfImages = ['fa-tree', 'fa-hand-spock-o', 'fa-paw', 'fa-sun-o', 'fa-moon-o', 'fa-paw', 'fa-leaf', 'fa-bicycle', 'fa-tree', 'fa-pagelines', 'fa-leaf','fa-pagelines', 'fa-sun-o', 'fa-bicycle', 'fa-hand-spock-o', 'fa-moon-o'];
 
 /*
  * Display the cards on the page
